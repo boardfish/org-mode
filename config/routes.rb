77c645526f1468a00744519c6ea055777da0630d
Simon Fish <si@mon.fish>
2017-09-05 17:57:26 +0100

    Fixed nav routes for new links page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
   resources :todos
-  root 'todos#index'
+  root 'home#index'
   get 'todos/:id/trello_delete' => 'todos#trello_delete', as: :trello_delete
   get 'todos/:id/trello_done' => 'todos#trello_done', as: :trello_done
 
