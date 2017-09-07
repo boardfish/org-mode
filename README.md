8aef43e6c0dc3b61f74e56cbd209a4fe88577516
Simon Fish <si@mon.fish>
2017-09-07 14:30:31 +0100

    Mild updates to examples to reflect changes

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Primarily a first attempt at Ruby on Rails. A personal organiser that hooks into
 
 - A todo and done list on Trello somewhere. They don't have to be on the same board.
 - A list of links on Trello, named as what they'll display as and with the link itself in the card description.
+- A GitHub account.
 
 ## Installation
 
--- example_api_configs.rb
+++ example_api_configs.rb
@@ -2,12 +2,14 @@ module Config
   GITHUB = {
     username: 'username',
     password: 'password'
-  }
+    access token: 'at the bottom of settings in github'
+  }.freeze
   TRELLO = {
     developer_public_key: 'developer_public_key',
     member_token: 'member_token',
     todo_list: 'list_long_id',
     done_list: 'list_long_id',
     links_list: 'list_long_id'
-  }
+  }.freeze
+  username = 'New user'.freeze
 end
